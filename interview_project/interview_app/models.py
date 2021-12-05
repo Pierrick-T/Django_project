4686a7c10d644b68f8a063b5c6ed2fce0e1afeae
Pierrick <pierrick.thomas@cpe.fr>
2021-12-05 17:14:42 +0100

    created database entries and fixed models

--- interview_project/interview_app/models.py
+++ interview_project/interview_app/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class PhysicalPort(models.Model):
+    location = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
 class User(models.Model):
+    physicalport = models.ForeignKey(PhysicalPort, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
-class PhysicalPort(models.Model):
-    # Add the Many to one relation here
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    location = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
